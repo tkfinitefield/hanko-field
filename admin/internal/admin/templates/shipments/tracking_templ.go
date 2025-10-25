a3c8c54c16fed00f86ec65b77a5bd9b749d67ecf
Toshiya Kazuyoshi <83720274+tkfinitefield@users.noreply.github.com>
2025-10-25 20:13:17 +0900

    fix

--- admin/internal/admin/templates/shipments/tracking_templ.go
+++ admin/internal/admin/templates/shipments/tracking_templ.go
@@ -302,7 +302,7 @@ func trackingBody(data TrackingPageData) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 23, "\" hx-indicator=\"#tracking-loading\" hx-swap=\"outerHTML\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 23, "\" hx-indicator=\"#tracking-loading\" hx-swap=\"innerHTML\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
